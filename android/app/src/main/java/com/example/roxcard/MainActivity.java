fc7ab412901fa954ce093a84e06123307555a210
Get X <154667275+askirya@users.noreply.github.com>
2025-04-10 01:20:13 +0300

    Delete android/app/src/main/java/com/example/roxcard/MainActivity.java

--- android/app/src/main/java/com/example/roxcard/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.roxcard;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-    // Ничего больше не требуется для Flutter embedding v2
-} 
